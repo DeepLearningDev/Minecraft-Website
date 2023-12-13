a57d3ce5d29ed7aa4d7fa49fc7b6d549869c6f98
K-eli-wtech <kewhite3@my.waketech.edu>
2023-12-13 12:29:51 -0500

    Template home page

---
+++ src/components/home-page.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="home">
+    <!-- Title Bar Section -->
+    <header>
+      <!-- Navigation or Title Bar -->
+      <!-- You can place your server's name, logo, or navigation links here -->
+      <h1>My Minecraft Server</h1>
+      <!-- Add navigation links if needed -->
+    </header>
+
+    <!-- Horizontal Cycling Images Section -->
+    <section class="image-slider">
+      <!-- Horizontal image slider or cycling images -->
+      <!-- Use a carousel or image slider component to display cycling images -->
+      <!-- Example: -->
+      <div class="slider">
+        <img src="path/to/image1.jpg" alt="Image 1">
+        <img src="path/to/image2.jpg" alt="Image 2">
+        <!-- Add more images -->
+      </div>
+    </section>
+
+    <!-- Embedded Video Section -->
+    <section class="video">
+      <!-- Embedded video from YouTube or other platforms -->
+      <!-- Example: -->
+      <div class="video-container">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allowfullscreen></iframe>
+        <!-- Replace VIDEO_ID with the ID of your YouTube video -->
+      </div>
+    </section>
+
+    <!-- News, Patch Notes, and Updates Section -->
+    <section class="updates">
+      <!-- News, Patch Notes, and Updates content -->
+      <h2>Latest News and Updates</h2>
+      <!-- Example: -->
+      <div class="news">
+        <h3>Recent Patch Notes</h3>
+        <ul>
+          <li><a href="/patches/patch1">Patch 1 - Title</a></li>
+          <li><a href="/patches/patch2">Patch 2 - Title</a></li>
+          <!-- Add more patch notes or news -->
+        </ul>
+      </div>
+      <!-- You can add more sections for news, updates, or server announcements -->
+    </section>
+
+    <!-- Footer Section -->
+    <footer>
+      <!-- Your footer content -->
+      <!-- Example: -->
+      <p>&copy; 2023 YourMinecraftServerName. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  // Add any necessary Vue component logic or methods
+}
+</script>
+
+<style scoped>
+/* Add scoped styles for your index.vue component */
+/* Customize styling based on your design preferences */
+/* Example: */
+.home {
+  /* Your home page styles */
+}
+
+/* Styles for different sections */
+.header {
+  /* Styles for title bar or header section */
+}
+
+.image-slider {
+  /* Styles for the horizontal image slider */
+}
+
+.video {
+  /* Styles for the embedded video section */
+}
+
+.updates {
+  /* Styles for news, patch notes, and updates section */
+}
+
+footer {
+  /* Styles for the footer */
+}
+</style>
+
