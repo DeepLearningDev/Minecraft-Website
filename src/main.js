a0aed0faee48188b5e1d25156c483a1e2046db66
K-eli-wtech <kewhite3@my.waketech.edu>
2023-12-13 14:10:54 -0500

    router established

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import VueRouter from 'vue-router';
 
 import HomePage from './pages/home-page.vue';
 import NewsPage from './pages/news-page.vue';
+import WikiPage from './pages/wiki-page.vue';
+import DownloadsPage from './pages/downloads-page.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/news', component: NewsPage }
+  { path: '/news', component: NewsPage },
+  { path: '/wiki', component: WikiPage},
+  { path: '/downloads', component: DownloadsPage }
   // Add more routes for additional pages as needed
 ];
 
