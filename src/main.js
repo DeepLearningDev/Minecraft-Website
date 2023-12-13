233bbf434603fe0449795d2e67fdeb99d7ab6224
K-eli-wtech <kewhite3@my.waketech.edu>
2023-12-13 14:03:44 -0500

    changes to file structure

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
 
-createApp(App).mount('#app')
+import HomePage from './pages/home-page.vue';
+import NewsPage from './pages/news-page.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/news', component: NewsPage }
+  // Add more routes for additional pages as needed
+];
+
+const router = new VueRouter({
+  mode: 'history', // Use 'history' mode or 'hash' mode for routing
+  routes
+});
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app');
